cabf65dc8ff6163115babbe215afa51c234b7fe3
Facundo <facundo.padilla@outlook.com>
2021-11-09 14:07:40 -0300

    :globe_with_meridians: Main application

--- api_rest_clicoh/settings.py
+++ api_rest_clicoh/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n*ck3!ru$ku-a*idko2nu3995^*blug$vgci$dq*=#g&#82gi^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
